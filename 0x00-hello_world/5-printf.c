8c2aa6d95c63e4b82f6e9afc19f83935a62bc3b6
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-16 17:40:29 -0800

    Task 5-printf.c completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Prints a line of code with the printf function
+ *
+ * Return: 0(Successful)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
