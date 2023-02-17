a139eeec70587b9bd90a89ea4b9f3e04404808fc
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-16 17:21:14 -0800

    Task 4. My first C program.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main() - prints a line of code using the puts() function
+ *
+ * Return: 0(Successful)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
