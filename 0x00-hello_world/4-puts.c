377127b647ffd230a3407e184bad26c6c3c0764d
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-16 16:56:21 -0800

    Task 4-puts.c completed. My first program in C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*This is my first task in C programming
+ *This task is to write a C program that prints exactly
+ *'Programming is like builking a multilingual puzzle'
+ *Using the pust() function.
+ *The return value is 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
