83eb3dac234e81e5186c3be39b75e8a66aa5cd35
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-17 20:46:58 -0800

    Task 5 completed and modified

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Prints integers in base
+ *
+ * Return: 0(Successful)
+*/
+
+int main(void)
+{
+	int a;
+	/* This is a loop */
+	for (a = 0; a <= 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
