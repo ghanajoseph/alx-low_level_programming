3a33f683ba7c8fb264882fcc382a143901283708
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-17 21:25:38 -0800

    Task 7 completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Prints a line of code
+ *
+ * Return: 0(Successful)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	/* This is the loop */
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
