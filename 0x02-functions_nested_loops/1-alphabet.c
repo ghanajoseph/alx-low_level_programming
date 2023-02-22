8ff285b7e6bd25b83e113ee5daf02973af5960fc
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-21 16:25:25 -0800

    Task 1 modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 /**
  * print_alphabet - function for printing alphabets in lower case
  *
- * return: returns 0
+ * return: returns 0 (Nothing per say but successful)
  */
 void print_alphabet(void)
 {
 	char i;
-
+	/* This is a for loop to print the alphabetsone at a tim */
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
