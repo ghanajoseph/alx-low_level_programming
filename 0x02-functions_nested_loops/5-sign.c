25d30084d38f3a64c54f753bbab3f8cab9bcfc20
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-21 18:18:53 -0800

    Task 5 completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * int print_sign - Prints signs
+ *
+ * @n: accepted parameter
+ *
+ * Return: Returns 1 if n is greater than zero,
+ * also returns 0 if n is zero
+ * and -1 when n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+}
