57d342df0708b76e36a796f643b4b8ec6f197d4f
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-21 16:05:46 -0800

    Starting project 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main -Prints a line of code.
+ *
+ * _putchar: print the alphabets putchar
+ *
+ * Return: 0(Successful)
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
