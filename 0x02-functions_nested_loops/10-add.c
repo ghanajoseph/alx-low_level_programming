f53f02b8b664c0b0d5ead051a631862734ec43c1
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-21 21:11:00 -0800

    Task 10 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - To add two digits
+ *
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: Returns an integer value
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
