37f2c0caec3fc32a41a27085a9c6e434207bea2f
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-23 18:36:23 -0800

    Task 8 completed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Prints n squares basedon n number of times
+ * @size: Number of squares
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
