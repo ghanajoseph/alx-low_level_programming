1b5807f76c62b322eec5426289213b73c6619e58
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-23 17:29:07 -0800

    Task 4 Modified

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,18 @@
 
 void print_most_numbers(void)
 {
-	int q;
+	int q = 0;
 
 	for (; q <= 9, q++)
 	{
-		if (q != 2 && q != 4)
+		if (q == 2 || q == 4)
+		{
+			continue;
+		}
+		else
 		{
 			_putchar(q + '0');
 		}
 	}
 	_putchar('\n');
-	return (0);
 }
