25499525084e09f8a7beb4c98652353f3cf0a5ff
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-23 17:20:42 -0800

    Task 4 completed

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints number from 0 to 9 except 2 and 4
+ *
+ * Return: 0(Successful)
+ */
+
+void print_most_numbers(void)
+{
+	int q;
+
+	for (; q <= 9, q++)
+	{
+		if (q != 2 && q != 4)
+		{
+			_putchar(q + '0');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
