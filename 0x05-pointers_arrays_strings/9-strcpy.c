6d05ee304aa575fcb893191df42dd560d0802ccb
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-02-27 16:06:53 -0800

    Task 9 completed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to
+ * @dest: Destination value
+ * @src: source value
+ * Return: pointer to desti
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+
+	return (0);
+}
