e8b8d893813737a75b0d775223adf32c507a7dd8
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-02 02:27:19 -0800

    Task 0 completed

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate given strings
+ *
+ * @dest: First string to use in the concatenation
+ * @src: Second string to use in the concatenation
+ * Return: Returns @dest parameter
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
