5ba36bc2f53742463670d4d857f4bc02e9093e23
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-02 11:06:57 -0800

    Task 4 completed

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -21,7 +21,7 @@ void print_array(int *a, int n)
 			printf(", ");
 		}
 		printf("%d", a[i]);
-		i++
+		i++;
 	}
 	printf("\n");
 }
@@ -32,8 +32,8 @@ void print_array(int *a, int n)
  * Return: Always 0.
  */
 int main(void)
-{
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337}}
+
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
 	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
