e10b1bf104c7956cb010654750213fff490311c3
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-06 11:04:39 -0800

    Task 100 completed

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,7 +11,7 @@ int main(void)
 	char *s0 = "Bob Dylan";
 	char *s1 = "Robert Allen";
 
-	printf("%s, %\n", s0, s1);
+	printf("%s, %s\n", s0, s1);
 	set_string(&s1, s0);
 	printf("%s, %s\n", s0, s1);
 	return (0);
