fadf3afe6c77d7cb4b1eeebb34c5f87cdcb07339
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-08 13:31:06 -0800

    Task 7 completed

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: input string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comp - compares each characterof string
+ * @s: input string
+ * @n1: smallest
+ * @n2: biggest
+ * Return: 0 (successful)
+ */
+int comp(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comp(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - returns 1 if string is palindrom
+ * @s: input string
+ * Return: 1 if palindrome and 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comp(s, 0, _strlen_recursion(s) - 1));
+}
