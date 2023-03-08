d921e1340ececdf342d19b3a66b7520d8041909e
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-08 12:13:30 -0800

    Task 2 completed

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ *
+ * @s: input string
+ * Return: 0 (Successful)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
