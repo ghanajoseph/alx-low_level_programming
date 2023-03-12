fe944b467ec847b9cb495843bd04c272a26db4af
Joseph Agbavitor <ghanajoseph@gmail.com>
2023-03-11 17:49:39 -0800

    Task 3 completed well

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - Converts a string to an integer
  * @s: string to be converted
- * Return: the int converted from the string
+ * Return: The int converted from the string
  */
 
 int _atoi(char *s)
@@ -17,6 +17,7 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
@@ -39,4 +40,29 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+
+/**
+ * main - multiplies two integers
+ * @argc: number of arguments
+ * @argv: The array of arguments
+ * Return: 0 (ssucceful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
