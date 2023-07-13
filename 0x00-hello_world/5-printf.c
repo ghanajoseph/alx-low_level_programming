1ad8557fb953edd3de416cd5da71355b572f24b3
Joseph <ghanajoseph@gmail.com>
2023-07-13 09:53:20 -0700

    Task 5 completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - A C program that prints with the printf() function
+*
+* Return: returns 0 on success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
