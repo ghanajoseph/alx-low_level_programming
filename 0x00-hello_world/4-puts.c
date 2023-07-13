59e50eb7b48c20f4df337bb605ca905350e488e8
Joseph <ghanajoseph@gmail.com>
2023-07-13 09:39:37 -0700

    Task 4 completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A C program to print with the puts() function
+ *
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
