79aa5523c1c0fb3a4d0351ebf635b5600ce822cd
Joseph <ghanajoseph@gmail.com>
2023-07-13 10:11:31 -0700

    Task 6 completed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - A C program that prints size of various types
+*
+* Return: Returns 0 on success.
+*/
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
+
