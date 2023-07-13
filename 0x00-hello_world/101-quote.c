8131e75ea8e2cc9d11ec05181b2afcb31f6f2abb
Joseph <ghanajoseph@gmail.com>
2023-07-13 14:44:43 -0700

    Task 8 completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - A C program taht prints without using printf or puts
+*
+* Return: Returns 1
+*/
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+return (1);
+}
+
