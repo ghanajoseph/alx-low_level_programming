9c3be388a04201338a34693cd203a8e05898de9a
Joseph <ghanajoseph@gmail.com>
2023-07-14 09:56:03 -0700

    Task 3 completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - A C program that prints alphabets in lowercase & uppercase
+*
+* Return: Returns 0 on success
+*/
+
+int main(void)
+{
+	char chl;
+	char chu;
+	for (chl = 'a'; chl <= 'z'; chl++)
+	{
+		putchar(chl);
+	}
+	for (chu = 'A'; chu <= 'Z'; chu++)
+	{
+		putchar(chu);
+	}
+	putchar('\n');
+	return (0);
+}
