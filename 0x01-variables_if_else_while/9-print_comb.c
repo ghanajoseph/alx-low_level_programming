bc68930575079f9e5e85ed9862caba06635ae603
Joseph <ghanajoseph@gmail.com>
2023-07-14 16:31:22 -0700

    Task 9 completed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - A C program prints possible ncombination of  single
+*
+* Return: Returns 2 on success
+*/
+
+int main(void)
+{
+	/* Creating variables for the project */
+	int a;
+
+	/* Looping through */
+	for (a = 0; a <= 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+putchar('\n');
+return (0);
+}
