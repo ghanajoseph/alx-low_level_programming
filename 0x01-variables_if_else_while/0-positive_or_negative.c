f84ceebeecfba5e89ba6b3ca35c5998a75f9687d
Joseph <ghanajoseph@gmail.com>
2023-07-14 08:40:40 -0700

    Task 0 completed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	/* Testing the content of n if its positive, zero or negative */
 	if (n > 0)
 		printf("%d is positive\n", n);
@@ -23,6 +22,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
