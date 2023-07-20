1dc4175f43242bc3a456df3f0392076629b3509f
Joseph <ghanajoseph@gmail.com>
2023-07-20 06:35:39 -0700

    Task 10 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * add - A C function that adds two integers
+  *
+  * @i: First input integer
+  * @j: Second input integer
+  *
+  * Return: Returns result.
+  */
+
+int add(int i, int j)
+{
+	/* Initialization of variable */
+	int k = i + j;
+
+	return (k);
+}
