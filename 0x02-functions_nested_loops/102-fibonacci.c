9453bc4cb5dd762b4bbf6a71681894443d5e6c15
Joseph <ghanajoseph@gmail.com>
2023-07-20 07:18:56 -0700

    Task 14 completed

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - A C program prints Fabonacci numbers
+  *
+  * Return: Returns 0 on success.
+  */
+
+int main(void)
+{
+	/* Initialization of variables for the program */
+	int i = 0;
+	int x = 1, y = 2;
+
+	/* Looping through the numbers */
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", x);
+		else if (i == 1)
+			printf(", %ld", y);
+		else
+		{
+			y += x;
+			x = y - x;
+			printf(", %ld", y);
+		}
+		++i;
+	}
+}
