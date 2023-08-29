0d8bf9eac96f7a0f902b6ac2837d8ce00635a00e
ghanajoseph <ghanajoseph@gmail.com>
2023-08-29 14:36:06 -0700

    Task 100 modified

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,27 +3,27 @@
 /**
   * reverse_listint - A C function reverses a listint_t linked list.
   * @head: Pointer to first node of the linked list.
-  * Return Returns a pointer to the first node of the reversed list
+  * Return: Returns a pointer to the first node of the reversed list
   */
 
 listint_t *reverse_listint(listint_t **head)
 {
 	/* Declaring & initializing pointers and variable */
-	listint_t *pv_NODE = NULL, *cur_NODE = *head, *nxt_NODE = NULL;
+	listint_t *pv_NODE = NULL, *nxt_NODE = NULL;
 	/*unsigned int i;*/
 
 	/* Checking if head pointer is NULL or not */
 	if (*head == NULL)
 		return (NULL);
 
-	while (cur_NODE != NULL)
+	while (*head)
 	{
-		nxt_NODE = cur_NODE->next;
-		cur_NODE->next = pv_NODE;
-		pv_NODE = cur_NODE;
-		cur_NODE = nxt_NODE;
+		nxt_NODE = (*head)->next;
+		(*head)->next = pv_NODE;
+		pv_NODE = *head;
+		*head = nxt_NODE;
 	}
 	*head = pv_NODE;
 
-	return (pv_NODE);
+	return (*head);
 }
