e9e1c5a56a1224df640c9ac333389afcc74999b7
ghanajoseph <ghanajoseph@gmail.com>
2023-09-04 13:50:31 -0700

    Task 1 modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,10 +26,6 @@ int create_file(const char *filename, char *text_content)
 		for (length = 0; text_content[length];)
 			length++;
 	}
-	else
-	{
-		return (-1);
-	}
 
 	/* Open file in READ-WRITE MODE */
 	/* Creates the file in 0600 permissions if it does not exist */
